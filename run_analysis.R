b8599b76b2271a37af25456dd17b060b5348f426
briantcs <briantcs88@gmail.com>
2014-09-20 11:57:50 +0800

    completed

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,6 @@
-##Reading x,y, subject in test folders with names as xtest, ytest, subjecttest respectively
-
-     xtest<-read.table("./test/X_test.txt",stringsAsFactors=F,fill=T)
-
-     ytest<-read.table("./test/y_test.txt",stringsAsFactors=F,fill=T)
-
-     subjecttest<-read.table("./test/subject_test.txt",stringsAsFactors=F,fill=T)
-     
-##Reading x,y, subject in train folders with names as xtrain, ytrain, subjecttrain respectively
-
-     xtrain<-read.table("./train/X_train.txt",stringsAsFactors=F,fill=T)
-
-     ytrain<-read.table("./train/y_train.txt",stringsAsFactors=F,fill=T)
-
-     subjecttrain<-read.table("./train/subject_train.txt",stringsAsFactors=F,fill=T)
-
-## Combine the 3 test data files into testdata
-    
-     testdata<-cbind(id,xtest,ytest,subjecttest)
-
-## Combine the 3 test data files into testdata
-     
-     traindata<-cbind(id,xtrain,ytrain,subjecttrain)
-     
-## Merge testdata and traindata into one single file called MergeData
-     MergeData<-rbind(testdata,traindata)
-     
-## End of part 1
-     
-
-
+require(knitr)
+require(markdown)
+# set working directoy
+setwd("~/Getting_cleaning_data_project")
+knit("run_analysis.Rmd", encoding="ISO8859-1")
+markdownToHTML("run_analysis.md", "run_analysis.html")
